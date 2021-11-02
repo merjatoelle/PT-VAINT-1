9f1656448d22eae982fffa47f3be1b4ff890dd02
Evgenii Churiulin <51716145+EvgenyChur@users.noreply.github.com>
2021-11-02 14:38:55 +0100

    The module for preprocessing of COSMO-CLM data
    Current Code Owner: CESR, Evgenii Churiulin
    phone:  +49  561 804-6142
    fax:    +49  561 804-6116
    email:  evgenychur@uni-kassel.de

--- cosmo_data.py
+++ cosmo_data.py
@@ -72,47 +72,25 @@ def get_data(data_path, parameter_name):
 #------------------------------------------------------------------------------
 # The subroutine needs for getting actual COSMO data
 # 
-# Input parameters : main_path - general path for research project
-#                    sf_path   - name of subfolder for COSMO data  
+# Input parameters : sf_path   - path for COSMO data  
 #                    fn_prefix - name of COSMO run (original or experiment)
 #                    clm_name  - names of COSMO parameters   
 #
 # Output parameters: df_cosmo - the data frame with information about COSMO data
 #------------------------------------------------------------------------------
-def cosmo_data(main_path, sf_path, fn_prefix, clm_name):
-    # mian path
-    mf_com = main_path
-    # subfolder
-    sf_cosmo = sf_path
-    # the main prefix
-    fn_cosmo = fn_prefix
-    # Name of columns and files
-    columns_name = clm_name   
-    #--------------------------------------------------------------------------
-    # Define actual filename for COSMO data
-    #--------------------------------------------------------------------------
-    fn_list = []
-    for fn in range(len(columns_name)):
-        name_file = columns_name[fn] + fn_cosmo
-        fn_list.append(name_file)
-    #--------------------------------------------------------------------------
-    # Define paths for COSMO data 
-    #--------------------------------------------------------------------------   
-    iPath = [] 
-    for i in range(len(columns_name)):
-        path = mf_com + sf_cosmo + fn_list[i]
-        iPath.append(path)    
-    #--------------------------------------------------------------------------   
-    # Get data
-    #--------------------------------------------------------------------------     
-    df_list = []
-    for j in range(len(iPath)):
-        data =  get_data(iPath[j], columns_name[j])     
-        df_list.append(data)            
-    df_cosmo = pd.concat(df_list, axis = 1)
+def cosmo_data(sf_path, fn_prefix, clm_name):
+    # list with COSMO data --> timeseries
+    cosmo_data = []
+
+    for param in clm_name:
+        filename = f'{param}{fn_prefix}'
+        path     = f'{sf_path}{filename}'
+        cosmo_data.append(get_data(path, param))                               # use COSMO function --> get_data
+    df_cosmo = pd.concat(cosmo_data, axis = 1)
     return df_cosmo  
 # end def cosmo_data
 #------------------------------------------------------------------------------
+
 
 #------------------------------------------------------------------------------
 # Subroutine: get_timeseries
@@ -120,63 +98,57 @@ def cosmo_data(main_path, sf_path, fn_prefix, clm_name):
 # The subroutine needs for getting actual timeseries from COSMO dataframes
 # 
 # Input parameters : clm_name - name of COSMO parameters 
-#                    data_ts  - columns name from list 
+#                    df_cosmo  - columns name from list 
 #                    period   - time period 
 #                    ts       - step for resampling
 #
 # Output parameters: parc_list - the list of COSMO parameters 
 #------------------------------------------------------------------------------
-def get_timeseries(clm_name, data_ts, period, ts):   
+def get_timeseries(df_cosmo, clm_name, period, ts):   
+    
     t0melt = 273.15
 
-    parc_list = []
-    for i in range(len(clm_name)):
-        if ts == None:
-            # Date without resemple
-            if clm_name[i] in ('ALHFL_BS','ALHFL_PL','ALHFL_S', 'ASHFL_S'):
-                par_parc01 = data_ts[clm_name[i]][period] * -1.0
-            elif clm_name[i] in ('AEVAP_S'):
-                par_parc01 = data_ts[clm_name[i]][period] * -1.0               
-            elif clm_name[i] in ('T_2M','T_S', 'TMAX_2M', 'TMIN_2M'):
-                par_parc01 = data_ts[clm_name[i]][period] - t0melt
-            elif clm_name[i] == 'PS':
-                #par_parc01 = (data_ts[clm_name[i]][period] / 100.0).interpolate()                        # [hPa]
-                par_parc01 = data_ts[clm_name[i]][period] / 100.0
-            elif clm_name[i] in ('ZTRALEAV', 'ZTRANG', 'ZTRANGS', 'ZVERBO'):
-                par_parc01 = data_ts[clm_name[i]][period] * -1.0 * 10e4
-            elif clm_name[i] in ('RSTOM'):
-                for j in range(len(data_ts[clm_name[i]])):
-                    if data_ts[clm_name[i]][j] > 20000.0:
-                        data_ts[clm_name[i]][j] = 20000.0                                           
-                par_parc01 = data_ts[clm_name[i]][period]               
-            else:
-                par_parc01 = data_ts[clm_name[i]][period]                       
+    cosmo_data = []
+    
+    for param in clm_name:
+        # Date where we have to use resample step
+        if param in ('ALHFL_BS', 'ALHFL_PL', 'ALHFL_S', 'ASHFL_S'):
+            new_data = df_cosmo[param][period].resample(ts).mean() * -1.0        
+             
+        elif param in ('T_2M', 'T_S', 'TMAX_2M', 'TMIN_2M'):
+            new_data = df_cosmo[param][period].resample(ts).mean() - t0melt             
+        
+        elif param in ('ZTRALEAV', 'ZTRANG', 'ZTRANGS', 'ZVERBO'):
+            new_data = df_cosmo[param][period].resample(ts).mean() * -1.0  * 10e4            
+        elif param == 'PS':
+            #new_data = ((data_ts[param][period].resample(ts).mean()) / 100.0).interpolate()  # [hPa]
+            new_data = (df_cosmo[param][period].resample(ts).mean()) / 100.0    # [hPa]          
+        
+        elif param == 'AEVAP_S':
+            new_data = df_cosmo[param][period].resample(ts).sum()  * -1.0  
+    
+        elif param == 'RSTOM':
+            # correct the stomatal resistance data
+            for j in range(len(df_cosmo[param])):
+                if df_cosmo[param][j] > 20000.0:
+                    df_cosmo[param][j] = 20000.0    
+            # calculate mean values                                      
+            new_data = df_cosmo[param][period].resample(ts).mean()         
+
         else:
-            # Date where we have to use resample step
-            if clm_name[i] in ('ALHFL_BS','ALHFL_PL','ALHFL_S', 'ASHFL_S'):
-                par_parc01 = data_ts[clm_name[i]][period].resample(ts).mean()  * -1.0
-            elif clm_name[i] in ('AEVAP_S'):
-                par_parc01 = data_ts[clm_name[i]][period].resample(ts).sum()  * -1.0
-            elif clm_name[i] in ('T_2M', 'T_S', 'TMAX_2M', 'TMIN_2M'):
-                par_parc01 = data_ts[clm_name[i]][period].resample(ts).mean() - t0melt                
-            elif clm_name[i] == 'PS':
-                #par_parc01 = ((data_ts[clm_name[i]][period].resample(ts).mean()) / 100.0).interpolate()  # [hPa]
-                par_parc01 = (data_ts[clm_name[i]][period].resample(ts).mean()) / 100.0  # [hPa]                
-            elif clm_name[i] in ('ZTRALEAV', 'ZTRANG', 'ZTRANGS', 'ZVERBO'):
-                par_parc01 = data_ts[clm_name[i]][period].resample(ts).mean() * -1.0  * 10e4          
-            elif clm_name[i] in ('RSTOM'):
-                for j in range(len(data_ts[clm_name[i]])):
-                    if data_ts[clm_name[i]][j] > 20000.0:
-                        data_ts[clm_name[i]][j] = 20000.0                                           
-                par_parc01 = data_ts[clm_name[i]][period].resample(ts).mean()         
-            else:
-                par_parc01 = data_ts[clm_name[i]][period].resample(ts).mean()
+            new_data = df_cosmo[param][period].resample(ts).mean()            
         
-        parc_list.append(par_parc01)
-        
-    return parc_list
+        cosmo_data.append(new_data)
+    
+    df = pd.concat(cosmo_data, axis = 1)
+    
+    return df                
 # end def get_timeseries
 #------------------------------------------------------------------------------
+
+
+
+
 
 #------------------------------------------------------------------------------
 # Subroutines: cosmo_montly_data, cosmo_daily_data, cosmo_hourly_data
@@ -196,75 +168,79 @@ def get_timeseries(clm_name, data_ts, period, ts):
 # Output parameters: dataframe - dataframe with mean parameters 
 #------------------------------------------------------------------------------
 
-def cosmo_montly_data(clm_name, data, period, ts):   
-    data_cosmo = get_timeseries(clm_name, data, period, ts)             
-    df_parc = pd.concat(data_cosmo, axis = 1)
-    df_parc = df_parc.reset_index()
-    mon_id  = df_parc['index'].dt.month
-    parc_m  = df_parc.groupby(mon_id).mean()
+def cosmo_montly_data(data, clm_name, period, ts):   
+    df     = get_timeseries(data, clm_name, period, ts)             
+    df     = df.reset_index()
+    mon_id = df['index'].dt.month                                              # get the current number for each row based on timeindex
+    #mon_id  = df_parc['Date'].dt.month
+    df_montly = df.groupby(mon_id).mean()
     # Re-indexing
     time_index = pd.date_range('1/1/2019', '12/1/2019', freq='MS').strftime('%B')
-    parc_m.index  = time_index    
-    return parc_m
+    df_montly.index  = time_index    
+    return df_montly
 
-def cosmo_daily_data(t_1, t_2, clm_name, data, ts):    
-    list_cosmo_years = []
-    list_cosmo_param = []    
-    for  tr in range(len(t_1)):
-        # Get a time period
-        period = pd.date_range(t_1[tr], t_2[tr], freq = '1H')            
-        parc   = get_timeseries(clm_name, data, period, ts)  
-        # Concat data in data frame for each year
-        df_year = pd.concat(parc, axis = 1)
-        # Add year to general list for all year
-        list_cosmo_years.append(df_year)   
-    # Concat all year in on dataframe
-    df_parc = pd.concat(list_cosmo_years, axis = 1)
+
+
+
+def cosmo_daily_data(data, clm_name, periods, ts):    
+    # Combine COSMO data for different monthes in one dataframe
+    cosmo_years = []   
+    for  index in range(len(periods)):
+        df = get_timeseries(data, clm_name, periods[index], ts)                  
+        cosmo_years.append(df)                                                 # Add year to general list for all year
+    df_period = pd.concat(cosmo_years, axis = 1)                               # Concat all year in on dataframe
+    
     # Reset index
-    df_parc = df_parc.reset_index()
+    df_period = df_period.reset_index()    
     # Get day index in June 
-    d_id    = df_parc['index'].dt.day
-    for i in range(len((clm_name))):
+    day_index = df_period['index'].dt.day   
+    # Get mean values for simular days
+    cosmo_param = []  
+    for param in clm_name:
         # Get data for each parameter
-        param = df_parc[clm_name[i]].groupby(d_id).mean()
-        param = param.mean(axis = 1)
-        param = param.rename(clm_name[i])   
+        new_data = df_period[param].groupby(day_index).mean()
+        new_data = new_data.mean(axis = 1)
+        new_data = new_data.rename(param)   
         # Add data to lists     
-        list_cosmo_param.append(param)       
+        cosmo_param.append(new_data)       
     # Get data    
-    df_cosmo = pd.concat(list_cosmo_param, axis = 1)   
+    df_cosmo = pd.concat(cosmo_param, axis = 1)   
     return df_cosmo
 
 
-def cosmo_hourly_data(t_1h, t_2h, clm_name, data):    
-    list_cosmo_years = []
-    list_cosmo_param  = []    
-    for  tr in range(len(t_1h)):
-        # Get a time period
-        period = pd.date_range(t_1h[tr], t_2h[tr], freq = '1H')            
-        ts = None
-        parc   = get_timeseries(clm_name, data, period, ts)  
-        # Concat data in data frame for each year
-        df_year = pd.concat(parc, axis = 1)
-        # Add year to general list for all year
-        list_cosmo_years.append(df_year)    
-    # Concat all year in on dataframe
-    df_parc = pd.concat(list_cosmo_years, axis = 1)
+
+
+def cosmo_hd(data, clm_name, periods, ts):    
+    # Combine COSMO data for different monthes in one dataframe   
+    cosmo_years = []
+    for  index in range(len(periods)):
+        df = get_timeseries(data, clm_name, periods[index], ts)  
+        cosmo_years.append(df)    
+    df_period = pd.concat(cosmo_years, axis = 1)                                 # Concat all year in on dataframe
+
     # Reset index
-    df_parc = df_parc.reset_index()
-    # Get day index in June 
-    d_id    = df_parc['index'].dt.hour
-    for i in range(len((clm_name))):
+    df_period = df_period.reset_index()    
+    # Get hour index
+    hour_index = df_period['index'].dt.hour
+    # Get mean values for simular hours
+    cosmo_param  = []     
+    for param in clm_name:
         # Get data for each parameter
-        param = df_parc[clm_name[i]].groupby(d_id).mean()
-        param = param.mean(axis = 1)
-        param = param.rename(clm_name[i])    
+        new_data = df_period[param].groupby(hour_index).mean()
+        new_data = new_data.mean(axis = 1)
+        new_data = new_data.rename(param)    
         # Add data to lists     
-        list_cosmo_param.append(param)       
+        cosmo_param.append(new_data)       
     # Get data    
-    df_cosmo = pd.concat(list_cosmo_param, axis = 1)   
+    df_cosmo = pd.concat(cosmo_param, axis = 1)   
     return df_cosmo
 #------------------------------------------------------------------------------
+
+
+
+
+
+
 
 #------------------------------------------------------------------------------
 # Subroutine: stat_cosmo
@@ -330,3 +306,71 @@ def stat_cosmo(clm_name, df_ctr, df_mod):
     return df_stat_cosmo
 
 
+
+"""
+def stat_cosmo(clm_name, df_ctr, df_mod):
+    # Create a nan timeseries
+    mean_orig = pd.Series(np.nan, index = range(len(clm_name)))
+    max_orig  = pd.Series(np.nan, index = range(len(clm_name))) 
+    min_orig  = pd.Series(np.nan, index = range(len(clm_name)))
+    mean_exp  = pd.Series(np.nan, index = range(len(clm_name)))
+    max_exp   = pd.Series(np.nan, index = range(len(clm_name)))
+    min_exp   = pd.Series(np.nan, index = range(len(clm_name)))
+    std_orig  = pd.Series(np.nan, index = range(len(clm_name)))
+    std_exp   = pd.Series(np.nan, index = range(len(clm_name)))
+    mae       = pd.Series(np.nan, index = range(len(clm_name)))
+    rmse      = pd.Series(np.nan, index = range(len(clm_name)))
+    corr      = pd.Series(np.nan, index = range(len(clm_name)))
+    
+    # Start calculations
+    for i in range(len(clm_name)):
+        mean_orig[i] = df_ctr[clm_name[i]].mean()
+        max_orig[i]  = df_ctr[clm_name[i]].max()
+        min_orig[i]  = df_ctr[clm_name[i]].min()
+        std_orig[i]  = df_ctr[clm_name[i]].std()
+        mean_exp[i]  = df_mod[clm_name[i]].mean()
+        max_exp[i]   = df_mod[clm_name[i]].max()
+        min_exp[i]   = df_mod[clm_name[i]].min()
+        std_exp[i]   = df_mod[clm_name[i]].std()       
+        mae[i]       = mean_absolute_error(df_ctr[clm_name[i]], df_mod[clm_name[i]])
+        rmse[i]      = mean_squared_error(df_ctr[clm_name[i]], df_mod[clm_name[i]])
+        corr[i]      = df_ctr[clm_name[i]].corr(df_mod[clm_name[i]])
+    
+    # Create data frame
+    df_stat_cosmo = pd.DataFrame({'Parameter' : ['AEVAP_S',  'ALHFL_S', 
+                                                 'ASHFL_S',  'RSTOM'  ,
+                                                 'ZVERBO' ,  'T_2M'   ,
+                                                 'T_S'    ,  'TMAX_2M',
+                                                 'TMIN_2M','ZTRALEAV'],
+                                  'Mean COSMO': mean_orig,
+                                  'Mean Model': mean_exp ,
+                                  'Max COSMO' : max_orig ,
+                                  'Max MODEL' : max_exp  ,
+                                  'Min COSMO' : min_orig ,
+                                  'Min MODEL' : min_exp  ,
+                                  'STD COSMO' : std_orig ,
+                                  'STD_MODEL' : std_exp  ,
+                                        'MAE' : mae      ,
+                                       'RMSE' : rmse     ,
+                                       'CORR' : corr     })
+    
+    df_stat_cosmo.set_index('Parameter', inplace = True)
+    
+    return df_stat_cosmo
+"""
+
+
+
+
+
+
+def data4month(data, clm_name, periods, ts):    
+    # Combine COSMO data for different monthes in one dataframe
+    cosmo_years = []   
+    for  index in range(len(periods)):
+        df = get_timeseries(data, clm_name, periods[index], 'D') 
+        df = df.resample(ts).mean()                 
+        cosmo_years.append(df)                                                 # Add year to general list for all year
+    df_period = pd.concat(cosmo_years, axis = 0)  
+    df_period = df_period.reset_index()
+    return df_period           
